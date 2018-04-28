6f66e117168bb7c60a638210a2f7abdc1489ce69
v0m003i <vmamidi@walmartlabs.com>
2018-04-28 16:59:34 -0700

    test

--- src/Arrays/TwoSum.java
+++ src/Arrays/TwoSum.java
@@ -3,61 +3,102 @@ package Arrays;
 import java.util.*;
 
 /*
-//Given an array of integers, find two numbers such that they add up to a specific target
-//number
+1) Given an array of integers, find two numbers such that they add up to a specific target
+number
+2) find all unique pairs
+3) find a pair if the array is sorted (binary search or two pointer solution)
  */
 public class TwoSum {
 
-  public static int [] twoSum(int [] input, int target) {
-      Map<Integer, Integer> map = new HashMap();
-      for (int i : input) {
-          if(map.containsKey(i)) {
-              return new int[]{i,map.get(i)};
-          }
-          map.put(target-i,i);
-      }
-      return null;
-  }
+    public static int[] twoSum(int[] input, int target) {
+        Map<Integer, Integer> map = new HashMap();
+        for (int i : input) {
+            if (map.containsKey(i)) {
+                return new int[]{i, map.get(i)};
+            }
+            map.put(target - i, i);
+        }
+        return null;
+    }
 
-  public static List<Pair> twoSumAllPairs(int [] input, int target){
-      Map<Integer, Integer> map = new HashMap();
-      List<Pair> li = new ArrayList<>();
-      Set<Integer> set = new HashSet<>();
-      for (int i : input) {
-          if(map.containsKey(i)) {
-              if(!set.contains(i)) {
-                  li.add(new Pair(i, map.get(i)));
-                  set.add(i);
-                  set.add(map.get(i));
+    public static List<Pair> twoSumAllPairs(int[] input, int target) {
+        Map<Integer, Integer> map = new HashMap();
+        List<Pair> li = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        for (int i : input) {
+            if (map.containsKey(i)) {
+                if (!set.contains(i)) {
+                    li.add(new Pair(i, map.get(i)));
+                    set.add(i);
+                    set.add(map.get(i));
+                }
+                continue;
+            }
+            map.put(target - i, i);
+        }
+        return li;
+    }
+
+    static class Pair {
+        int a;
+        int b;
+
+        Pair(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+    }
+
+    public static int[] twoSumSorted(int[] nums, int target) {
+
+        for (int i : nums) {
+
+            if (binarySearch(nums, i, target)) {
+                return new int[]{i, target - i};
+            }
+
+        }
+        return null;
+    }
+
+
+
+  private static boolean binarySearch(int []nums,int i, int target){
+          int left=0,right=nums.length -1;
+          int mid= (left+right)/2;
+
+          while(left<right) {
+              if(nums[mid]==target-i) return true;
+              if(nums[mid]<target-i){
+                  left=mid+1;
+              }else {
+                  right=mid-1;
               }
+              mid=(left+right)/2;
           }
-          map.put(target-i,i);
+          return false;
       }
-      return li;
-  }
 
-  static class  Pair {
-      int a;
-      int b;
-      Pair(int a, int b){
-          this.a=a;
-          this.b=b;
-      }
-  }
   public static void main(String args []) {
-      int nums [] = {1,5,3,4,4,4,5,7,9};
-      int result [] = twoSum(nums,14);
+      int nums [] = {1,3,1,3,2,2,2,2};
+      int result [] = twoSum(nums,4);
       if(result != null)
       System.out.println("A Solution is: "+ result[0] + " " + result[1]);
       else System.out.println("No result found");
 
-      List<Pair> li = twoSumAllPairs(nums,10);
+      List<Pair> li = twoSumAllPairs(nums,4);
       if(li != null && !li.isEmpty()) {
           System.out.println("Num of solutions: " +li.size());
           for(Pair p : li) {
               System.out.println("solutions: " +p.a+" "+p.b);
           }
       }
+
+      int input [] = {1,2,3,4,5,6};
+      result = twoSumSorted(input,7);
+      if(result != null)
+          System.out.println("A Solution for sorted array is: "+ result[0] + " " + result[1]);
+      else System.out.println("No result found");
   }
 
 
